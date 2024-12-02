99e446d0808ab7de08519c84751c339d1348b241
Jonathan Zhang <69145546+jonathan-d-zhang@users.noreply.github.com>
2024-12-02 18:26:18 -0500

    Add cloudflare tunnel

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python -m alembic upgrade head && python -m fastapi run src/openbook/server.py --root-path /api
+python -m alembic upgrade head && python -m fastapi run src/openbook/server.py
